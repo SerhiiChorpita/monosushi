817e04fbf1b9793ac82f1cabd1b433446a466e97
SerhiiChorpita <sergiochorpita@gmail.com>
2022-08-22 09:02:56 +0200

    start new project MONOSUSHI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'monosushi',
+    appId: '1:126065826039:web:4849c928eae8806ee05bf2',
+    storageBucket: 'monosushi.appspot.com',
+    apiKey: 'AIzaSyBQjKRsYgJwQbHLqP_EAw-m_KqrwFPeBxQ',
+    authDomain: 'monosushi.firebaseapp.com',
+    messagingSenderId: '126065826039',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'monosushi',
+    appId: '1:126065826039:web:4849c928eae8806ee05bf2',
+    storageBucket: 'monosushi.appspot.com',
+    apiKey: 'AIzaSyBQjKRsYgJwQbHLqP_EAw-m_KqrwFPeBxQ',
+    authDomain: 'monosushi.firebaseapp.com',
+    messagingSenderId: '126065826039',
+  },
   production: false
 };
 
